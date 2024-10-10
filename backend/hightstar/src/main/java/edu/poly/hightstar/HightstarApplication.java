5bc174923a9b38de2cf9aaab58bac2c0a4685512
linhnguyen2209 <linhli2k4@gmail.com>
2024-10-10 09:24:32 +0700

    Initial commit for frontend and backend projects

---
+++ backend/hightstar/src/main/java/edu/poly/hightstar/HightstarApplication.java
@@ -0,0 +1,13 @@
+package edu.poly.hightstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HightstarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HightstarApplication.class, args);
+	}
+
+}
---
+++ backend/hightstar/src/test/java/edu/poly/hightstar/HightstarApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.poly.hightstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HightstarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
